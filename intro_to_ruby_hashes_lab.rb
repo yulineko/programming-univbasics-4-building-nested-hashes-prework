4d0862c406bc0f2078b20467e9d2cbcaa391d595
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-04 01:31:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -48,7 +48,7 @@ monopoly = {
           "mortgage_value" => "$100"
 	      },
         :pennsylvania_railroad => {
-          
+          "mortgage_value" => "$200"
         },
         :b_and_o_railroad => {
           
